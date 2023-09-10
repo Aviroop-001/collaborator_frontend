ca4cef9869dc71484ac399aae68568da76c77f0f
Aviroop-001 <banerjeeaviroop01@gmail.com>
2023-09-11 01:29:43 +0530

    fixed only-read page

--- src/components/ReadOnlyEditor.jsx
+++ src/components/ReadOnlyEditor.jsx
@@ -1,27 +1,55 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { useParams } from "react-router-dom";
+import api from "../api";
 
-const ReadOnlyEditor = ({ content }) => {
-  const editorRef = useRef(null);
-  const quillInstance = useRef(null);
+export default function ReadOnlyEditor() {
+  const { documentId } = useParams();
+  const [quill, setQuill] = useState();
+  const [doc, setDoc] = useState();
+
+  const fetchDocumentById = async () => {
+    try {
+      const response = await api.get(`document/${documentId}`);
+      if (response.status === 200) {
+        setDoc(response.data.content);
+      } else {
+        throw new Error("Failed to fetch document.");
+      }
+    } catch (error) {
+      console.error("Error fetching document:", error);
+      throw error;
+    }
+  };
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const quillInstance = new Quill("#editor", {
+      theme: "snow",
+      readOnly: true,
+    });
+    quillInstance.disable();
+    setQuill(quillInstance);
+    quillInstance.setText("Loading ..... ");
+    fetchDocumentById();
+  }, []);
 
   useEffect(() => {
-    if (editorRef.current && !quillInstance.current) {
-      quillInstance.current = new Quill(editorRef.current, {
-        theme: "snow",
-        readOnly: true,
-      });
+    if (quill == null) return;
+    quill.setContents(doc);
+  }, [doc]);
 
-      quillInstance.current.clipboard.dangerouslyPasteHTML(content);
-    }
-  }, [content]);
+
+
 
   return (
     <div>
-      <div ref={editorRef} style={{ height: "400px" }} />
+      <div ref={wrapperRef} id="editor" style={{ height: "400px" }} />
     </div>
   );
-};
-
-export default ReadOnlyEditor;
+}
